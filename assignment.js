5dd4a1ca36b9440e33ec3fcac8f14a3428d3e116
varshax11 <163232037+varshax11@users.noreply.github.com>
2024-10-04 13:31:08 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,23 @@
+function chocolate(arr){
+    let count=0;
+for(let i=0;i<N;i++){
+    if(arr[i]!=0){
+        arr[count++]=arr[i];
+    }
+}
+let val=arr.length-count;
+while(count<arr.length){
+    arr[count++]=0;
+}
+return {arr,val};
+
+}
+N=prompt("Enter the number of toys");
+const arr=[]
+for(let i=0;i<N;i++){
+    const v=prompt(`Enter the toy ${i+1}`);
+    arr.push(v);
+}
+let result=chocolate(arr);
+console.log(result.val);
+console.log(result.arr);
